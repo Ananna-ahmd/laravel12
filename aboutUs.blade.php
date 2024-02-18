2be11aee8a33450c40f0cfbd95a7e7e225dc39fb
Ananna-ahmd <157790634+Ananna-ahmd@users.noreply.github.com>
2024-02-18 16:56:36 +0600

    Add files via upload

---
+++ aboutUs.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Information</title>
+</head>
+<body>
+<button class=" button button2">About US</button>
+<a href="{{route('form')}}"button class=" button button2">About US</a>
+@csrf
+
+<p>
+
+In this website you can buy all your daily need like biscuit ,chocolate, icecream,coke .......
+</p>
+</body>
+</html>
