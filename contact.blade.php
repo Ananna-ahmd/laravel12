5fdb135657d098804e105700f51b9c5e14d08269
Ananna-ahmd <157790634+Ananna-ahmd@users.noreply.github.com>
2024-02-18 16:57:30 +0600

    Add files via upload

---
+++ contact.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Information</title>
+</head>
+<body>
+<button class=" button button3">CONTACT</button>
+<a href="{{route('form')}}"button class=" button button3">CONTACT</a>
+@csrf
+<div>
+    <li>
+        Phone No- 019155555
+    </li>
+    <li>
+        Email-Eshop@ghjg.gmail.com
+    </li>
+</div>
+
+
+
+
+</body>
+</html>
